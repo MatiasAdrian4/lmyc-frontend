be3cbc631ea356e64fdeb34ea586a40a3fd4aca7
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-09-09 20:45:58 -0300

    LMYC-71: re added env variable in next-config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    LMYC_BACKEND_HOST: process.env.LMYC_BACKEND_HOST
+  }
 }
 
 module.exports = nextConfig
