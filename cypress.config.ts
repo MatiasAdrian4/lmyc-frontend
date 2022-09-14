04cab86d900efb5096e79a2541c442191593894b
Matias Peñalba Paz <43422582+MatiasAdrian4@users.noreply.github.com>
2022-09-14 19:37:32 -0300

    LMYC-71: Configure e2e tests with Cypress (#23)
    * LMYC-71: cypress setup and login page tests
    
    * LMYC-71: added navigation bar e2e tests
    
    * LMYC-71: fix .gitignore
    
    * LMYC-71: run e2e tests in GA
    
    * LMYC-71: minor change
    
    * LMYC-71: fix e2e GA
    
    * LMYC-71: fix GA
    
    * LMYC-71: fix GA
    
    * LMYC-71: added timeout for requests
    
    * LMYC-71: removed .env file
    
    * LMYC-71: re added env variable in next-config file
    
    * LMYC-71: backend setup for running e2e tests
    
    * LMYC-71: added migration step
    
    * LMYC-71: added dependency
    
    * LMYC-71: merge steps
    
    * LMYC-71: reverted change
    
    * LMYC-71: fix GA using new django management command
    
    * LMYC-71: renamed step
    
    * LMYC-71: merge jobs
    
    * LMYC-71: fix
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: fixed GA
    
    * LMYC-71: disabled screenshot and video
    
    Co-authored-by: Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000/",
+    supportFile: "cypress/support/command.js"
+  },
+  env: {
+    lmyc_username: "test_user",
+    lmyc_password: "test_password"
+  },
+  defaultCommandTimeout: 10000,
+  video: false,
+  screenshotOnRunFailure: false
+})
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    LMYC_BACKEND_HOST: "http://localhost:8000/lubricentro_myc"
+    LMYC_BACKEND_HOST: process.env.LMYC_BACKEND_HOST
   }
 }
 
