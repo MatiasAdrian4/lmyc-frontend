49f2fed67c43ff4e0f8011543804217956c54a23
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-09-18 09:29:29 -0300

    LMYC-72: added section with invoice info

--- pages/invoices/[invoiceId].tsx
+++ pages/invoices/[invoiceId].tsx
@@ -1,16 +1,34 @@
 import { GetServerSideProps } from "next"
 import { InvoicesApi } from "../../api/lmycApi"
 import {
+  formatDate,
   getJWTFromCtx,
   isUserAuthenticated,
   ssRedirectToLoginPage
 } from "../../utils/utils"
+import styles from "../../styles/invoices/Invoice.module.css"
 
 export default function Invoice({ invoice }) {
   return (
     <>
-      <p>{JSON.stringify(invoice)}</p>
-      {/** Add the possibility to edit the current invoice items and add new ones */}
+      <h3 className={styles.sectionTitle}>Información del Remito</h3>
+      <div className={styles.section}>
+        <fieldset>
+          <label>Código</label>
+          <input readOnly={true} type="text" value={invoice.codigo} style={{ width: "100px" }}></input>
+          <label>Cliente</label>
+          <input readOnly={true} type="text" value={invoice.cliente} style={{ width: "200px" }}></input>
+          <label>Fecha</label>
+          <input
+            readOnly={true}
+            type="text"
+            value={formatDate(invoice.fecha)}
+            style={{ width: "100px" }}
+          ></input>
+        </fieldset>
+        <p>{JSON.stringify(invoice.resumen_elementos)}</p>
+        {/** Add the possibility to edit the current invoice items and add new ones */}
+      </div>
     </>
   )
 }
